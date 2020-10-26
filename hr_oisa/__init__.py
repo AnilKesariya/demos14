d299ef14ba1d64cb8ce25ad33ddcb48a0e407447
Magpemr12 <guadalupemendoza@mit-mut.com>
2020-10-25 20:47:27 -0600

    hr_oisa and pac_sa

---
+++ hr_oisa/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    AtharvERP Business Solutions
+#    Copyright (C) 2020-TODAY AtharvERP Business Solutions(<http://www.atharverp.com>).
+#    Author: AtharvERP Business Solutions(<http://www.atharverp.com>)
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from . import models
